aa7dfa8e51467fe6790cd7df6f5872f85288cdfd
joakibj <joakibj@gmail.com>
2013-07-22 01:36:02 +0200

    Changed to less hacky arg parsing. Added the command: export

--- tswrdb-cmdui/src/main/scala/com/joakibj/tswrdb/TswRdb.scala
+++ tswrdb-cmdui/src/main/scala/com/joakibj/tswrdb/TswRdb.scala
@@ -4,38 +4,60 @@ import rdb.export.RdbExporter
 import java.io.File
 import rdb.{RdbTypeNotFoundException, RdbType, RdbTypes}
 
-case class Config(rdbDataDirectory: File = new File("."), rdbType: Int = 0)
+object ListRdbTypesMode extends Enumeration {
+  val None, All, Understood = Value
+}
+
+case class Config(rdbDataDirectory: File = new File("."),
+                  rdbType: Int = 0,
+                  command: String = "",
+                  listMode: Enumeration#Value = ListRdbTypesMode.None)
 
 object TswRdb extends App {
   val parser = new scopt.OptionParser[Config]("tswrdb") {
     head("tswrdb", "0.1")
-    opt[File]('r', "rdb") required() valueName ("<directory>") action {
-      (file, config) =>
-        config.copy(rdbDataDirectory = file)
-    } text ("rdb points to the directory that has RDB files and is required.")
-    opt[Int]('t', "type") required() action {
-      (rtype, config) =>
-        config.copy(rdbType = rtype)
-    } text ("rdbType of the data that is going to be exported.")
     help("help") text ("prints this usage text.")
     version("version")
     note("Data is exported to the export folder.")
     cmd("list") action {
       (_, config) =>
-        listWellUnderstoodRbTypes()
-        config
+        config.copy(command = "list", listMode = ListRdbTypesMode.Understood)
     } text ("Lists the valid rdb types available. Per default and to keep the user sane, only well understood RdbTypes are listed.") children (
       opt[Unit]("all") abbr ("a") action {
         (_, config) =>
-          listAllRdbTypes()
-          config
+          config.copy(listMode = ListRdbTypesMode.All)
       } text ("List all rdbtypes, regardless. Note that some are highly mysterious and/or esoteric. You will have to make sense of them yourself")
+      )
+    cmd("export") action {
+      (_, config) =>
+        config.copy(command = "export")
+    } text ("Export entries belonging to this rdbtype") children(
+      opt[File]('r', "rdb") required() valueName ("<directory>") action {
+        (file, config) =>
+          config.copy(rdbDataDirectory = file)
+      } text ("rdb points to the directory that has RDB files and is required."),
+      arg[Int]("<rdbType>") required() action {
+        (rdbType, config) =>
+          config.copy(rdbType = rdbType)
+      } text ("rdbType of the data that is going to be exported.")
       )
   }
 
   parser.parse(args, Config()) map {
     config =>
-      startExport(config)
+      config.command match {
+        case "list" =>
+          config.listMode match {
+            case ListRdbTypesMode.All => listAllRdbTypes()
+            case ListRdbTypesMode.Understood => listWellUnderstoodRbTypes()
+            case _ => exit()
+          }
+        case "export" =>
+          startExport(config)
+        case _ =>
+          parser.showUsage
+          exit()
+      }
   } getOrElse {
 
   }
@@ -54,7 +76,7 @@ object TswRdb extends App {
       (rdbType) =>
         println("\t" + rdbType.id + "\t" + rdbType.name)
     }
-    System.exit(0)
+    exit()
   }
 
   def startExport(config: Config) {
@@ -62,17 +84,18 @@ object TswRdb extends App {
       val rdbType = RdbTypes.find(config.rdbType).getOrElse {
         throw new RdbTypeNotFoundException("RdbType: " + config.rdbType + " was not found")
       }
+      println("RDB data directory set to: " + config.rdbDataDirectory.getCanonicalPath)
       println("Exporting RdbType: " + rdbType + " into export/" + rdbType.id + " ...")
       RdbExporter(config.rdbDataDirectory).exportAll(config.rdbType)
     } catch {
-      case ex: RdbTypeNotFoundException => exit(ex.getMessage)
-      case ex: RuntimeException => exit(ex.getMessage)
+      case ex: RdbTypeNotFoundException => exit(ex.getMessage, 1)
+      case ex: RuntimeException => exit(ex.getMessage, 1)
     }
   }
 
-  def exit(msg: String) {
-    println(msg)
+  def exit(msg: String = "", exitCode: Int = 0) {
+    if (msg.length > 0) println(msg)
     println("Exiting...")
-    System.exit(1)
+    System.exit(exitCode)
   }
 }
