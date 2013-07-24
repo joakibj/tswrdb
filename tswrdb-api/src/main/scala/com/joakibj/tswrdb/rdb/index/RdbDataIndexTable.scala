7905ca9554b8b9e69fcf36c3893a06eca36e50d4
joakibj <joakibj@gmail.com>
2013-07-24 11:12:19 +0200

    Added copyright notice at the top of each source file, as recommended by GPLv2

--- tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/index/RdbDataIndexTable.scala
+++ tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/index/RdbDataIndexTable.scala
@@ -1,3 +1,12 @@
+/*
+ * tswrdb - a program and API to export the TSW resource database
+ *
+ * Copyright (C) 2013 Joakim Bjørnstad <joakibj@gmail.com>
+ *
+ * Licensed under the GNU General Public License version 2.
+ * Please see the LICENSE file for the license text in verbatim.
+ */
+
 package com.joakibj.tswrdb.rdb.index
 
 import collection.mutable.ArrayBuffer
