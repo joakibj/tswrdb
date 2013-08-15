d4e990014ed97fece0236972d9092060b95515a4
joakibj <joakibj@gmail.com>
2013-08-15 02:49:02 +0200

    Moved all language table reading from client to client code in RdbStringDataExporter
    This is so clients of RdbStringDataExporter do not have to worry about
    reading the language table

--- tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/strings/RdbStringDataExporter.scala
+++ tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/strings/RdbStringDataExporter.scala
@@ -11,28 +11,51 @@ package com.joakibj.tswrdb.rdb.strings
 
 import java.io.File
 import com.joakibj.tswrdb.rdb.export.{DataFileWriter, RdbDataEntry, RdbDataExporter}
-import com.joakibj.tswrdb.rdb.{RdbIOException, RdbType}
+import com.joakibj.tswrdb.rdb.{RdbTypes, RdbIOException, RdbType}
+import com.joakibj.tswrdb.rdb.index.RdbIndexEntry
 
-object RdbStringDataExporter {
-  def apply(rdbFilename: String, stringIndexTable: List[RdbStringLangIndexEntry]) =
-    new RdbStringDataExporter(new File(rdbFilename), stringIndexTable)
-
-  def apply(rdbDataDirectory: File, stringIndexTable: List[RdbStringLangIndexEntry]) =
-    new RdbStringDataExporter(rdbDataDirectory, stringIndexTable)
+object StringExportFormat extends Enumeration {
+  val Xml = Value("xml")
+  val Json = Value("json")
 }
 
-class RdbStringDataExporter(rdbDataDirectory: File, stringIndexTable: List[RdbStringLangIndexEntry]) extends RdbDataExporter(rdbDataDirectory) {
-  val postDataTransformer = new RdbStringDataXmlTransformer
+object RdbStringDataExporter {
+  def apply(rdbFilename: String, language: StringLanguage.Value, exportFormat: StringExportFormat.Value) =
+    new RdbStringDataExporter(new File(rdbFilename), language, exportFormat)
+
+  def apply(rdbDataDirectory: File, language: StringLanguage.Value, exportFormat: StringExportFormat.Value) =
+    new RdbStringDataExporter(rdbDataDirectory, language, exportFormat)
+}
+
+class RdbStringDataExporter(rdbDataDirectory: File, language: StringLanguage.Value, exportFormat: StringExportFormat.Value) extends RdbDataExporter(rdbDataDirectory) {
+  val postDataTransformer = exportFormat match {
+    case StringExportFormat.Xml => new RdbStringDataXmlTransformer
+    case StringExportFormat.Json => new RdbStringDataJsonTransformer
+  }
+  val languageTable = getLanguageTable()
+
+  def exportStrings() {
+    exportFiltered(RdbTypes.Strings, (ie: RdbIndexEntry) => languageTable.map(_.rdbId).contains(ie.id))
+  }
 
   protected def exportDataToFile(rdbType: RdbType, outputDirectory: File, dataEntry: RdbDataEntry, buf: Array[Byte]) {
-    val filename = mapToCanonicalName(dataEntry.id) + ".xml"
+    val filename = mapToCanonicalName(dataEntry.id) + "." + exportFormat
     val fileWriter = DataFileWriter(new File(outputDirectory, filename))
     fileWriter.writeData(buf)
   }
 
   private def mapToCanonicalName(dataEntryId: Int) = {
-    stringIndexTable.find((stringLangIndexEntry) => stringLangIndexEntry.rdbId == dataEntryId).getOrElse {
+    languageTable.find((stringLangIndexEntry) => stringLangIndexEntry.rdbId == dataEntryId).getOrElse {
       throw new RdbIOException("String DataEntry did not have a matching Category")
     }.category.name
+  }
+
+  private def getLanguageTable() = {
+    val languageTableFileName = "../Data/text/" + language + ".tdbl"
+    val stringLanguageReader = new RdbStringLanguageIndexReader(new File(rdbDataDirectory, languageTableFileName), language)
+    val languageTable = stringLanguageReader.readEntries()
+    stringLanguageReader.close()
+
+    languageTable
   }
 }
