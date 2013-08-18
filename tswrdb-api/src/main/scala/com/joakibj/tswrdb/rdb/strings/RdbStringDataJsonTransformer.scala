fd4e3d7272c199a639c53b75523c1451e2d54f62
joakibj <joakibj@gmail.com>
2013-08-18 21:52:27 +0200

    Refactor. Avoid use of return. Changed if return to if-else

--- tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/strings/RdbStringDataJsonTransformer.scala
+++ tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/strings/RdbStringDataJsonTransformer.scala
@@ -18,12 +18,13 @@ class RdbStringDataJsonTransformer extends RdbDataTransformer {
     val stringRdbReader = new RdbStringFileReader(buf)
     val (_, strings) = stringRdbReader.getStrings()
 
-    if (strings.length == 0) return new Array[Byte](0)
-
-    val mapper = new ObjectMapper()
-    mapper.registerModule(DefaultScalaModule)
-    mapper.configure(SerializationFeature.INDENT_OUTPUT, true)
-
-    mapper.writeValueAsBytes(strings)
+    if (strings.length == 0)
+      new Array[Byte](0)
+    else {
+      val mapper = new ObjectMapper()
+      mapper.registerModule(DefaultScalaModule)
+      mapper.configure(SerializationFeature.INDENT_OUTPUT, true)
+      mapper.writeValueAsBytes(strings)
+    }
   }
 }
