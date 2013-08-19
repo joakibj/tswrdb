719a0ce0fcb12e9a01ba70d05b67a7706b61ab05
Joakim Bjørnstad <joakibj@gmail.com>
2013-08-19 13:43:04 +0200

    Change RdbIndexEntry to a case class.
    To solve content equality between two RdbIndexEntry, the datatype of hash was changed to Seq[Byte]. Since it would refer to a WrappedArray through implicit conversion if an Array was passed.

--- tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/index/RdbDataIndexTable.scala
+++ tswrdb-api/src/main/scala/com/joakibj/tswrdb/rdb/index/RdbDataIndexTable.scala
@@ -23,40 +23,17 @@ case class RdbIndexHeader(version: Int,
 }
 
 object RdbIndexEntry {
-  def apply(rdbType: Int, id: Int, fileNum: Byte, dataOffset: Int, length: Int, hash: Array[Byte]) =
-    new RdbIndexEntry(rdbType, id, fileNum, dataOffset, length, hash)
-
   def apply(tup2: Tuple2[Int, Int], tup4: Tuple4[Byte, Int, Int, Array[Byte]]) =
     new RdbIndexEntry(tup2._1, tup2._2, tup4._1, tup4._2, tup4._3, tup4._4)
 }
 
-class RdbIndexEntry(val rdbType: Int,
+case class RdbIndexEntry(val rdbType: Int,
                     val id: Int,
                     val fileNum: Byte,
                     val dataOffset: Int,
                     val length: Int,
-                    val hash: Array[Byte]) {
+                    val hash: Seq[Byte]) {
   val fileName = "%02d.rdbdata" format fileNum
-
-  override def equals(other: Any) = other match {
-    case that: RdbIndexEntry => {
-      this.rdbType == that.rdbType &&
-        this.id == that.id &&
-        this.fileNum == that.fileNum &&
-        this.dataOffset == that.dataOffset &&
-        this.length == that.length
-    }
-    case _ => false
-  }
-
-  override def toString = {
-    "(type: " + rdbType +
-      ", id: " + id +
-      ", fileNum: " + fileNum +
-      ", fileName: " + fileName +
-      ", dataOffset: " + dataOffset +
-      ", length: " + length + ")"
-  }
 }
 
 object RdbDataIndexTable {
